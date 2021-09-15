1b023d96c7fbead41befc02c11b38a27261343f1
jasper <s218005@students.aie.edu>
2021-09-15 12:00:12 -0500

    this is a new class that was made (its for the entity)

---
+++ BattleArena/Entity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleArena
+{
+    class Entity
+    {
+        private string _name;
+        private float _health;
+        private float _attackPower;
+        private float _defensePower;
+
+
+        public Entity()
+        {
+            _name = "Default";
+            _health = 0;
+            _attackPower = 0;
+            _defensePower = 0;
+        }
+
+        public Entity(string name, float health, float attackPower, float defensePower)
+        {
+            _name = name;
+            _health = health;
+            _attackPower = attackPower;
+            _defensePower = defensePower;
+        }
+    }
+}
